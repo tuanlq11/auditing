edcb4df3498698a33989f4e909eac8eaf3ef7ddd
TuanLe <tuan.le@exe.com.vn>
2016-03-31 10:48:23 +0700

    Init structure

---
+++ database/migrations/2020_12_31_9999_create_audit_table.php
@@ -0,0 +1,26 @@
+<?php
+
+use \Illuminate\Database\Migrations\Migration;
+
+class CreateAuditTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('audit', function (Blueprint $table) {
+            $table->char('id', 32);
+            $table->string('model', 255);
+            $table->string('model_id', 255);
+            $table->integer('user_id')->nullable();
+            $table->text('new_value')->nullable();
+            $table->text('old_value')->nullable();
+
+            $table->primary('id');
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::drop('audit');
+    }
+}
---
+++ src/AuditingProvider.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace tuanlq11\AuditingProvider;
+
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Support\ServiceProvider;
+
+class AuditingProvider extends ServiceProvider
+{
+
+    /**
+     *
+     */
+    public function boot()
+    {
+        $this->initMigration($this->app);
+    }
+
+    /**
+     *
+     */
+    public function register()
+    {
+        // TODO: Implement register() method.
+    }
+
+    /**
+     * Copy migration to resources
+     *
+     * @param Application $app
+     */
+    private function initMigration(Application $app)
+    {
+        if ($app instanceof \Illuminate\Foundation\Application && $app->runningInConsole()) {
+            $migrationPath = realpath(__DIR__ . '/../database/migrations');
+            $this->publishes([$migrationPath => database_path('migrations')]);
+        }
+    }
+
+}
